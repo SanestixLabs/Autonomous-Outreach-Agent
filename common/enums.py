f7898bccdd3a122955a58bc9bf597f7504b3a83c
Sanestix <sanestix@gmail.com>
2026-08-04 22:46:17 +0500

    Revert "feat: implement workflow router"

--- common/enums.py
+++ common/enums.py
@@ -6,4 +6,3 @@ class WorkflowType(str, Enum):
     LEAD_QUALIFICATION = "Lead Qualification"
     SERVICE_RECOMMENDATION = "Service Recommendation"
     PERSONALIZED_EMAIL = "Personalized Email"
-    CRM_SYNC = "CRM Sync"
